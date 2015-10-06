1c4690d707b9e2e77154feafeb08664347a7e1b0
oktobercomstock <oktober.comstock@gmail.com>
2015-10-06 19:16:17 +0000

    Updated README.md

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,18 @@ class Item < ActiveRecord::Base
   
    default_scope { order('expires_at ASC') }
   
-   def days_left
-      7 - (DateTime.now.to_date - created_at.to_date).to_i
-   end
-   
+    def days_left
+     result = 7 - (DateTime.now.to_date - created_at.to_date).to_i
+
+       if result > 0
+         return result
+       else
+         return "Expired"
+       end
+    end
+    
+    def delete_items
+      #this will delete the items that are created 7 days ago from today.
+       self.where('created_at =?', 7.days.ago).destroy_all
+    end
 end
